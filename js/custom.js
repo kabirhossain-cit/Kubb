e78d34059a9b9a31e65cd58c11d7f427a9212bec
kabirhossain-cit <kabirhossain.cit@gmail.com>
2021-10-07 11:28:11 +0600

    update

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,33 @@
 $(function(){
+
+    // ========================= preloader =======================
     $(window).on('load', function(){
-        $('.preloader').delay(1000).fadeOut(1000);
+        $('.preloader').delay(500).fadeOut(500);
+    });
+// // ====================== preloader end ============================
+
+// // ====================== back to top============================
+
+    $('.back-to-top').click(function(){
+        $('html, body').animate({scrollTop:0},1200);
+    });
+
+    $(window).scroll(function(){
+        var scrolling = $(this).scrollTop();
+        if(scrolling > 300){
+            $('.back-to-top').fadeIn(500);
+        }
+        else {
+            $('.back-to-top').fadeOut(500);
+        }
+        if(scrolling > 300){
+            $('.nav').addClass('.bg');
+        }
     });
 });
+// ====================== back to top end============================
+
+
+
+
+
